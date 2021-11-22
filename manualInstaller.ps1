7ffc376b1e8b2e6f2e5faccafa57a533d0d9eb87
demerara151 <demeraraonfire@gmail.com>
2021-11-22 21:12:15 +0900

    refactoring codes

--- manualInstaller.ps1
+++ manualInstaller.ps1
@@ -7,7 +7,7 @@ else {
     Set-Location $portable
 }
 
-'ES', 'wpd' | ForEach-Object { New-Item -Name $_ -ItemType Directory }
+('ES', 'WPD').ForEach{ New-Item -Name $_ -ItemType Directory}
 
 Invoke-WebRequest -Uri "https://dforest.watch.impress.co.jp/library/c/clibor/10713/clibor.zip" -OutFile $portable\clibor.zip
 
@@ -18,16 +18,12 @@ Invoke-WebRequest -Uri "https://wpd.app/get/latest.zip" -OutFile $portable\wpd\l
 Write-Output `n'$env:Path+=";$HOME\PortableApps\ES"' >> $Profile
 '$env:Path+=";$HOME\PortableApps\wpd"' >> $Profile
 
-7z x .\clibor.zip
+7z x clibor.zip
 
-Set-Location .\ES\
-7z e .\ES-1.1.0.20.zip
+Set-Location ES | 7z e ES-1.1.0.20.zip
 
-Set-Location ..\wpd\
-7z e .\latest.zip
+Set-Location ../wpd | 7z e latest.zip
 
-Set-Location ..
-
-Get-ChildItem -Include *.zip -Recurse | Remove-Item
+Set-Location .. | Get-ChildItem -Include *.zip -Recurse | Remove-Item
 
 Write-Output `n"Note: You need to restart Powershell to apply chaneges"
