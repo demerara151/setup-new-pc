276e6573557d6a2d27a03cfe1bafdb708e1452ea
demerara151 <demeraraonfire@gmail.com>
2022-01-17 02:22:46 +0900

    update old programs and add new one

--- AutoInstallerFull.ps1
+++ AutoInstallerFull.ps1
@@ -5,7 +5,7 @@ scoop bucket add games
 scoop bucket add nonportable
 
 # Must-Have tools
-scoop install autohotkey bat bitwarden bottom broot dust everything everything-cli fd ffmpeg fzf gitui less lightbulb lsd monolith mpv neovim posh-git ripgrep sd sharex starship sudo sumatrapdf yt-dlp zenhan zoxide
+scoop install autohotkey bat bitwarden bottom broot dust everything everything-cli fd ffmpeg fzf gitui less librewolf lightbulb lsd monolith mpv neovim pwsh posh-git ripgrep sd sharex starship sudo sumatrapdf yt-dlp zenhan zoxide
 
 # Programming（Optional from here. You can put `#` on top of the line to disable install）
 scoop install deno github julia rustup-msvc sqlitebrowser
@@ -14,17 +14,21 @@ scoop install deno github julia rustup-msvc sqlitebrowser
 scoop install legendary playnite
 
 # Media
-scoop install foobar2000-portable freetube gallery-dl streamlink streamlink-twitch-gui
+scoop install foobar2000 foobar2000-encoders freetube gallery-dl streamlink streamlink-twitch-gui
 
 # Nerd Fonts
 sudo scoop install VictorMono-NF FantasqueSansMono-NF-Mono
 
 # Install with winget
 winget install gsudo
-winget install LibreWolf.LibreWolf
-winget install Microsoft.Powershell
 winget install Python.Python.3
+winget install Microsoft.VisualStudio.2022.BuildTools
+winget install Microsoft.VC++2015-2022Redist-x86
+winget install Microsoft.VC++2015-2022Redist-x64
+winget install Microsoft.dotnetRuntime.5-x64
+winget install Microsoft.dotnetRuntime.6-x64
 winget install Discord.Discord
 winget install Valve.Steam
+winget install HeroicGamesLauncher.HeroicGamesLauncher
 winget install DuongDieuPhap.ImageGlass
 winget install Foundry376.Mailspring
