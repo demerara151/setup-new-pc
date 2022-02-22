014a264eb0d6f5b752a1fbc83bb6641081528c9a
demerara151 <demeraraonfire@gmail.com>
2022-02-22 17:15:23 +0900

    add more functionalities

--- InstallAll.ps1
+++ InstallAll.ps1
@@ -1,3 +1,11 @@
+# Stop and Disable Services
+sudo Set-Service -Name Spooler -StartupType Disabled -Status Stopped
+sudo Set-Service -Name WSearch -StartupType Disabled -Status Stopped
+sudo Set-Service -Name edgeupdate -StartupType Disabled -Status Stopped
+sudo Set-Service -Name edgeupdatem -StartupType Disabled -Status Stopped
+sudo Set-Service -Name MicrosoftEdgeElevationService -StartupType Disabled -Status Stopped
+
+
 # Adding buckets
 foreach (
     $bucket in @(
@@ -8,8 +16,9 @@ foreach (
     )
 ) { scoop bucket add $bucket }
 
+
 # Must-Have tools
-scoop install autohotkey bat bitwarden bottom broot dust everything everything-cli fd ffmpeg fzf gitui less lightbulb lsd monolith mpv neovim posh-git ripgrep sd sharex starship sudo sumatrapdf yt-dlp zenhan zoxide
+scoop install aria2 autohotkey bat bitwarden bottom broot czkawka dust everything everything-cli fd ffmpeg fzf gitui less lightbulb lsd monolith mpv neovim posh-git ripgrep sd sharex starship sudo shutup10 sumatrapdf tldr windowsspyblocker yt-dlp zenhan zoxide
 
 # Programming（Optional from here. You can put `#` on top of the line to disable install）
 scoop install github python rustup-msvc sqlitebrowser
@@ -26,20 +35,24 @@ sudo scoop install VictorMono-NF-Mono FantasqueSansMono-NF-Mono CascadiaCode-NF-
 # Display thumbnail
 sudo scoop install icaros-np
 
+
 # Check whether winget command is installed
 if (!(Get-Command winget -ErrorAction Continue)) { scoop install winget }
 
 # Install with winget
 $programs = @(
     "gerardog.gsudo",
+
     "Microsoft.PowerShell",
     "LibreWolf.LibreWolf"
     "Foundry376.Mailspring",
     "DuongDieuPhap.ImageGlass",
+
     "Microsoft.dotnetRuntime.6-x64",
     "Microsoft.VC++2015-2022Redist-x64",
     "Microsoft.VC++2015-2022Redist-x86",
     "Microsoft.VisualStudio.2022.BuildTools",
+
     "Valve.Steam",
     "Discord.Discord",
     "HeroicGamesLauncher.HeroicGamesLauncher"
@@ -48,3 +61,10 @@ $programs = @(
 foreach ($program in $programs) {
     winget install --id $program -s winget
 }
+
+
+# Install WPD
+mkdir $HOME\WPD | Set-Location
+Invoke-WebRequest -Uri "https://wpd.app/get/latest.zip" -OutFile $HOME\WPD\wpd.zip
+7z x wpd.zip
+.\WPD.exe
