10a5c6ceb59b59b4d60757c5deec1c0b6b84739f
demerara151 <demeraraonfire@gmail.com>
2022-02-22 13:12:01 +0900

    refactor script to use foreach function

--- InstallAll.ps1
+++ InstallAll.ps1
@@ -1,8 +1,12 @@
 # Adding buckets
-scoop bucket add extras
-scoop bucket add nerd-fonts
-scoop bucket add games
-scoop bucket add nonportable
+foreach (
+    $bucket in @(
+        "extras",
+        "nerd-fonts",
+        "games",
+        "nonportable"
+    )
+) { scoop bucket add $bucket }
 
 # Must-Have tools
 scoop install autohotkey bat bitwarden bottom broot dust everything everything-cli fd ffmpeg fzf gitui less lightbulb lsd monolith mpv neovim posh-git ripgrep sd sharex starship sudo sumatrapdf yt-dlp zenhan zoxide
@@ -26,15 +30,21 @@ sudo scoop install icaros-np
 if (!(Get-Command winget -ErrorAction Continue)) { scoop install winget }
 
 # Install with winget
-winget install gsudo
-winget install Microsoft.PowerShell
-winget install LibreWolf.LibreWolf
-winget install Foundry376.Mailspring
-winget install DuongDieuPhap.ImageGlass
-winget install Microsoft.dotnetRuntime.6-x64
-winget install Microsoft.VC++2015-2022Redist-x64
-winget install Microsoft.VC++2015-2022Redist-x86
-winget install Microsoft.VisualStudio.2022.BuildTools
-winget install Valve.Steam
-winget install Discord.Discord
-winget install HeroicGamesLauncher.HeroicGamesLauncher
+$programs = @(
+    "gsudo",
+    "Microsoft.PowerShell",
+    "LibreWolf.LibreWolf"
+    "Foundry376.Mailspring",
+    "DuongDieuPhap.ImageGlass",
+    "Microsoft.dotnetRuntime.6-x64",
+    "Microsoft.VC++2015-2022Redist-x64",
+    "Microsoft.VC++2015-2022Redist-x86",
+    "Microsoft.VisualStudio.2022.BuildTools",
+    "Valve.Steam",
+    "Discord.Discord",
+    "HeroicGamesLauncher.HeroicGamesLauncher"
+)
+
+foreach ($program in $programs) {
+    winget install $program -s winget
+}
