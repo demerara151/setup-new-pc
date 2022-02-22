29c06a6a6ae408202075bf3b56267e38b0863c61
demerara151 <demeraraonfire@gmail.com>
2022-02-22 10:23:56 +0900

    add missing apps

--- AutoInstallerFull.ps1
+++ AutoInstallerFull.ps1
@@ -5,13 +5,13 @@ scoop bucket add games
 scoop bucket add nonportable
 
 # Must-Have tools
-scoop install autohotkey bat bitwarden bottom broot dust everything everything-cli fd ffmpeg fzf gitui less lightbulb lsd monolith mpv neovim pwsh posh-git ripgrep sd sharex starship sudo sumatrapdf yt-dlp zenhan zoxide
+scoop install autohotkey bat bitwarden bottom broot dust everything everything-cli fd ffmpeg fzf gitui less lightbulb lsd monolith mpv neovim posh-git ripgrep sd sharex starship sudo sumatrapdf yt-dlp zenhan zoxide
 
 # Programming（Optional from here. You can put `#` on top of the line to disable install）
 scoop install github python rustup-msvc sqlitebrowser
 
 # Gaming
-scoop install legendary playnite
+scoop install legendary playnite osulazer
 
 # Media
 scoop install foobar2000 foobar2000-encoders freetube gallery-dl
@@ -19,15 +19,19 @@ scoop install foobar2000 foobar2000-encoders freetube gallery-dl
 # Nerd Fonts
 sudo scoop install VictorMono-NF-Mono FantasqueSansMono-NF-Mono
 
+# Display thumbnail
+sudo scoop install icaros-np
+
 # Install with winget
 winget install gsudo
 winget install Microsoft.PowerShell
 winget install LibreWolf.LibreWolf
-winget install Microsoft.VisualStudio.2022.BuildTools
-winget install Microsoft.dotnetRuntime.5-x64
-winget install Microsoft.dotnetRuntime.6-x64
-winget install Discord.Discord
-winget install Valve.Steam
-winget install HeroicGamesLauncher.HeroicGamesLauncher
-winget install DuongDieuPhap.ImageGlass
 winget install Foundry376.Mailspring
+winget install DuongDieuPhap.ImageGlass
+winget install Microsoft.dotnetRuntime.6-x64
+winget install Microsoft.VC++2015-2022Redist-x64
+winget install Microsoft.VC++2015-2022Redist-x86
+winget install Microsoft.VisualStudio.2022.BuildTools
+winget install Valve.Steam
+winget install Discord.Discord
+winget install HeroicGamesLauncher.HeroicGamesLauncher
