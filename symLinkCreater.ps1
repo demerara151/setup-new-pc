0e8bf3ffc8cf6e9e5130a84826ae5ebc5b41e8db
demerara151 <demeraraonfire@gmail.com>
2022-02-23 17:04:36 +0900

    add check PS version  function

--- symLinkCreater.ps1
+++ symLinkCreater.ps1
@@ -1,45 +1,49 @@
-# TODO: If you are using PowerShell 5.1, Raise warning to use PowerShell 7.2 and quit program.
-
-
-$CONFIG = "$HOME/.config"
-
-# bat
-sudo New-Item -ItemType SymbolicLink -Path $env:APPDATA/bat -Target $CONFIG/bat
-
-# broot
-$confHjson = "$env:APPDATA/dystroy/broot/config/conf.hjson"
-if (!(Test-Path $confHjson)) {
-    write-host "Warning!! Please run broot at least once!!" -ForegroundColor DarkRed
-    Write-Host "Next, Run the following command: \"Remove-Item $confHjson -Force\""
-    Write-Host "Finally, Run this: \"sudo New-Item -ItemType symboliclink -Path $confHjson -Target $CONFIG/broot/conf.hjson\""
+# ### PowerShell 7.2 is required ###
+if ($PSVersionTable.PSVersion.Major -lt 6) {
+    Write-Host "Error! Please use PowerShell 7.2 or higher!" -ForegroundColor DarkRed
 }
 else {
-    Remove-Item $confHjson -Force
-    sudo New-Item -ItemType symboliclink -Path $confHjson -Target $CONFIG/broot/conf.hjson
+    $CONFIG = "$HOME/.config"
+
+    # bat
+    sudo New-Item -ItemType SymbolicLink -Path $env:APPDATA/bat -Target $CONFIG/bat
+
+    # broot
+    $confHjson = "$env:APPDATA/dystroy/broot/config/conf.hjson"
+    if (!(Test-Path $confHjson)) {
+        write-host "Warning!! Please run broot at least once!!" -ForegroundColor DarkRed
+        Write-Host "Next, Run the following command: \"Remove-Item $confHjson -Force\""
+        Write-Host "Finally, Run this: \"sudo New-Item -ItemType symboliclink -Path $confHjson -Target $CONFIG/broot/conf.hjson\""
+    }
+    else {
+        Remove-Item $confHjson -Force
+        sudo New-Item -ItemType symboliclink -Path $confHjson -Target $CONFIG/broot/conf.hjson
+    }
+
+    # nushell
+    Remove-Item $env:APPDATA/nushell/nu/config -Recurse -Force
+    sudo New-Item -ItemType SymbolicLink -Path $env:APPDATA/nushell/nu/config -Target $CONFIG/nushell
+
+    # nvim
+    sudo New-Item -ItemType SymbolicLink -Path $env:LOCALAPPDATA/nvim -Target $CONFIG/nvim
+
+    # mpv
+    sudo New-Item -ItemType SymbolicLink -Path $HOME/scoop/persist/mpv/portable_config/mpv.conf -Target $CONFIG/mpv.conf
+
+    # powershell
+    $dotConfPath = "$CONFIG/PowerShell/Microsoft.Powershell_profile.ps1"
+    if (!(Test-Path $PROFILE)) {
+        New-Item -ItemType Directory -Path $HOME/Documents/PowerShell
+        sudo New-Item -ItemType SymbolicLink -Path $PROFILE -Target $dotConfPath
+    }
+    else {
+        Remove-Item $PROFILE -Force
+        sudo New-Item -ItemType SymbolicLink -Path $PROFILE -Target $dotConfPath
+    }
+
+    # vscodium
+    $setting = "$HOME/scoop/persist/vscodium/data/user-data/User"
+    Remove-Item $setting/settings.json -Force
+    sudo New-Item -ItemType SymbolicLink -Path $setting/settings.json -Target $CONFIG/codium/settings.json
+
 }
-
-# nushell
-Remove-Item $env:APPDATA/nushell/nu/config -Recurse -Force
-sudo New-Item -ItemType SymbolicLink -Path $env:APPDATA/nushell/nu/config -Target $CONFIG/nushell
-
-# nvim
-sudo New-Item -ItemType SymbolicLink -Path $env:LOCALAPPDATA/nvim -Target $CONFIG/nvim
-
-# mpv
-sudo New-Item -ItemType SymbolicLink -Path $HOME/scoop/persist/mpv/portable_config/mpv.conf -Target $CONFIG/mpv.conf
-
-# powershell
-$dotConfPath = "$CONFIG/PowerShell/Microsoft.Powershell_profile.ps1"
-if (!(Test-Path $PROFILE)) {
-    New-Item -ItemType Directory -Path $HOME/Documents/PowerShell
-    sudo New-Item -ItemType SymbolicLink -Path $PROFILE -Target $dotConfPath
-}
-else {
-    Remove-Item $PROFILE -Force
-    sudo New-Item -ItemType SymbolicLink -Path $PROFILE -Target $dotConfPath
-}
-
-# vscodium
-$setting = "$HOME/scoop/persist/vscodium/data/user-data/User"
-Remove-Item $setting/settings.json -Force
-sudo New-Item -ItemType SymbolicLink -Path $setting/settings.json -Target $CONFIG/codium/settings.json
