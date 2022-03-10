f619d4c67b95efe35047aaf4432e88e3fa186231
demerara151 <demeraraonfire@gmail.com>
2022-03-10 20:00:54 +0900

    change program order and set gsudo to run command

--- InstallAll.ps1
+++ InstallAll.ps1
@@ -1,15 +1,3 @@
-# Adding buckets
-foreach (
-    $bucket in @(
-        "extras",
-        "nerd-fonts"
-    )
-) { scoop bucket add $bucket }
-
-
-# Must-Have tools
-sudo scoop install aria2 autohotkey bat bottom broot dust fd ffmpeg fzf gitui less lsd mpv neovim nu posh-git ripgrep so starship shutup10 vscodium windowsspyblocker yt-dlp zenhan zoxide FantasqueSansMono-NF-Mono CascadiaCode-NF-Mono VictorMono-NF-Mono
-
 # Check whether winget command is installed
 if (!(Get-Command winget -ErrorAction Continue)) { scoop install winget }
 
@@ -20,12 +8,27 @@ $programs = @(
     "Microsoft.PowerShell",
     "LibreWolf.LibreWolf"
 
-    "Microsoft.VC++2015-2022Redist-x64"
+    "Microsoft.VC++2015-2022Redist-x64",
+    "Microsoft.dotnetRuntime.6-x64"
 )
 foreach ($program in $programs) {
     winget install --id $program -s winget
 }
 
+# --- gsudo start --- #
+gsudo
+
+# Adding buckets
+foreach (
+    $bucket in @(
+        "extras",
+        "nerd-fonts"
+    )
+) { scoop bucket add $bucket }
+
+
+# Must-Have tools
+scoop install aria2 autohotkey bat bottom broot dust fd ffmpeg fzf gitui less lsd mpv neovim nu posh-git ripgrep so starship shutup10 vscodium windowsspyblocker yt-dlp zenhan zoxide FantasqueSansMono-NF-Mono CascadiaCode-NF-Mono VictorMono-NF-Mono
 
 # Stop and Disable Services
 $Services = @(
@@ -41,9 +44,11 @@ $Services = @(
     "MicrosoftEdgeElevationService"
 )
 foreach ($name in $Services) {
-    sudo Set-Service -Name $name -StartupType Disabled -Status Stopped
+    Set-Service -Name $name -StartupType Disabled -Status Stopped
 }
 
+# --- gsudo end --- #
+exit
 
 # Set the .config directory
 Move-Item -Path "$HOME/setup-new-pc/.config/*" -Destination "$HOME/.config" -Force
