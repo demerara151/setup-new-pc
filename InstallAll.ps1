a786a1d80aafb36034f940e55649ec370ffa22dc
demerara151 <demeraraonfire@gmail.com>
2022-03-21 07:21:59 +0900

    change some installation for real env

--- InstallAll.ps1
+++ InstallAll.ps1
@@ -2,12 +2,29 @@
 foreach (
     $bucket in @(
         "extras",
-        "nerd-fonts"
+        "nerd-fonts",
+        "games",
+        "nonportable"
     )
 ) { scoop bucket add $bucket }
 
 # Must-Have tools
-sudo scoop install aria2 autohotkey bat bottom broot dust fd ffmpeg fzf gitui less lsd mpv neovim posh-git ripgrep starship shutup10 vscodium windowsspyblocker yt-dlp zenhan zoxide IBMPlexSans-JP FantasqueSansMono-NF-Mono CascadiaCode-NF-Mono VictorMono-NF-Mono
+scoop install aria2 autohotkey bat bitwarden bottom broot czkawka dust everything everything-cli fd ffmpeg fzf gitui less lightbulb lsd monolith mpv neovim nu posh-git ripgrep sd sharex so starship shutup10 sumatrapdf tldr vscodium windowsspyblocker yt-dlp zenhan zoxide
+
+# Programming（Optional from here. You can put `#` on top of the line to disable install）
+scoop install github python rustup-msvc sqlitebrowser
+
+# Gaming
+scoop install legendary playnite osulazer
+
+# Media
+scoop install foobar2000 foobar2000-encoders freetube gallery-dl
+
+# Nerd Fonts
+sudo scoop install IBMPlexSans-JP VictorMono-NF-Mono FantasqueSansMono-NF-Mono CascadiaCode-NF-Mono Mononoki-NF-Mono --global
+
+# Display thumbnail
+sudo scoop install icaros-np
 
 # Check whether winget command is installed
 if (!(Get-Command winget -ErrorAction Continue)) { scoop install winget }
@@ -17,10 +34,17 @@ $programs = @(
     "gerardog.gsudo",
 
     "Microsoft.PowerShell",
+    "DuongDieuPhap.ImageGlass",
     "LibreWolf.LibreWolf",
 
+    "Microsoft.dotnetRuntime.6-x64",
     "Microsoft.VC++2015-2022Redist-x64",
-    "Microsoft.dotnetRuntime.6-x64"
+    "Microsoft.VC++2015-2022Redist-x86",
+    "Microsoft.VisualStudio.2022.BuildTools",
+
+    "Valve.Steam",
+    "Discord.Discord",
+    "HeroicGamesLauncher.HeroicGamesLauncher"
 )
 foreach ($program in $programs) {
     winget install --id $program -s winget
