5248d0b425bb9db400dde13cba0c0935f7a782fa
demerara151 <demeraraonfire@gmail.com>
2022-04-19 09:04:12 +0900

    add missing filename in the path

--- symLinkCreator.ps1
+++ symLinkCreator.ps1
@@ -6,7 +6,7 @@ else {
     $CONFIG = "$HOME/.config"
 
     # autohotkey
-    sudo New-Item -ItemType SymbolicLink -Path "$($env:APPDATA)/Microsoft/Windows/Start Menu/Programs/Startup" -Target $CONFIG/autohotkey/keyswap.ahk
+    sudo New-Item -ItemType SymbolicLink -Path "$($env:APPDATA)/Microsoft/Windows/Start Menu/Programs/Startup/keyswap.ahk" -Target $CONFIG/autohotkey/keyswap.ahk
 
     # bat
     sudo New-Item -ItemType SymbolicLink -Path $env:APPDATA/bat -Target $CONFIG/bat
