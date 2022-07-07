7af05fb63d3a0fd569c464d6cf15b94e35c2791d
demerara151 <demeraraonfire@gmail.com>
2022-07-07 09:29:33 +0900

    [change:] install runtime via scoop instead of winget

--- installAll.ps1
+++ installAll.ps1
@@ -14,7 +14,7 @@ sudo scoop install vcredist-aio
 VisualCppRedist_AIO_x86_x64.exe /y  # use "/ai1" istead of "/y" to update all packages
 
 # Essentials
-scoop install aria2 autohotkey bat bitwarden bottom brave broot czkawka dust everything everything-cli fd ffmpeg ffsend fzf hwinfo imageglass less lightbulb lsd mailspring mpv neovim posh-git pwsh ripgrep sd sharex starship shutup10 terminal-icons trilium vscode yt-dlp zenhan zoxide
+scoop install aria2 autohotkey bat bitwarden bottom brave broot czkawka dust everything everything-cli fd ffmpeg ffsend fzf hwinfo imageglass less lightbulb lsd mailspring mpv neovim posh-git pwsh ripgrep sd sharex starship shutup10 terminal-icons trilium vscode windowsdesktop-runtime-lts yt-dlp zenhan zoxide
 
 # --- Optional from here. You can disable install by put `#` on top of each line. --- #
 # Programming
@@ -39,9 +39,6 @@ if (!(Get-Command winget -ErrorAction Continue)) { scoop install winget }
 
 # Install some requirements and softwares that you want to set system default with winget
 $programs = @(
-    # Requirement for lightbulb
-    "Microsoft.dotnetRuntime.6-x64",
-
     # Default Browser
     "LibreWolf.LibreWolf",
 
