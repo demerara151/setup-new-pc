890456104f9d18a87b0d30d58869a43f2e75e26f
demerara151 <demeraraonfire@gmail.com>
2022-10-25 08:49:12 +0900

    add description

--- stopService.ps1
+++ stopService.ps1
@@ -1,11 +1,41 @@
+<#
+    .SYNOPSIS
+    Stop and disable unnecessary services.
+
+    .DESCRIPTION
+    個人的に不要だなと思うサービスを停止し、無効化します。
+
+    メリットは、メモリの使用量をわずかに減らす程度なので、気にしなくてもいいレベルのものです。
+    使うことのないサービスが常駐してるのが気持ち悪い人向けです。
+
+    .NOTES
+    無効化したくないサービスには、行の先頭に # を付けてください。
+    サービス名が並んだ最終行の末尾に、カンマがある場合、取り除いてください。
+
+    Sophia Script や WPD, shutup10 等で無効化されないものだけを並べています。ある意味安全です。
+#>
+
+#Requires -RunAsAdministrator
+
 $services = @(
-    "Fax",
-    #    "iphlpsvc", # IP Helper
-    #    "IpxlatCfgSvc", # IP 変換構成サービス
+    # 外部デバイス
+    "Fax", # ファックス
+    "Spooler", # 印刷
+
+    # IPv6
+    "iphlpsvc", # IP Helper
+    "IpxlatCfgSvc", # IP 変換構成サービス
+
+    # 位置情報
     "lfsvc", # Geo location
-    "Spooler", # Print Spooler
+
+    # Xbox関連
     "XboxGipSvc",
+
+    # 標準検索（無効化しても検索はできるし十分速い）
     "WSearch", # Windows search
+
+    # Microsoft Edgeの自動アップデート（常駐型）
     "edgeupdate",
     "edgeupdatem",
     "MicrosoftEdgeElevationService"
