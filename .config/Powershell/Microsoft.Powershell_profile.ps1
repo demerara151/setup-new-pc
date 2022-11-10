ba256015a775f8785e5d8567fffd875a55cb14d0
demerara151 <demeraraonfire@gmail.com>
2022-11-10 20:56:16 +0900

    refactor powershell script

--- .config/Powershell/Microsoft.Powershell_profile.ps1
+++ .config/Powershell/Microsoft.Powershell_profile.ps1
@@ -1,11 +1,11 @@
 ﻿Import-Module -Name posh-git
 Import-Module Terminal-Icons
 
-# starship initialization
+# starship
 Invoke-Expression (&starship init powershell)
 
-# Exact search in `zi` (zoxide + fzf) command.
-# $env:_ZO_FZF_OPTS = "--exact"
+# Exact match when use `zi` (zoxide + fzf) command.
+$env:_ZO_FZF_OPTS = "--exact"
 
 # zoxide
 Invoke-Expression (& {
@@ -13,94 +13,200 @@ Invoke-Expression (& {
     (zoxide init --hook $hook powershell | Out-String)
     })
 
-# Aliases
-Set-Alias -Name vim -Value nvim
-Set-Alias -Name br -Value broot
-Set-Alias -Name mt -Value marktext
-
 # CONSTANTS
 $LIST = "$HOME\list.txt"
 $CONFIG = "$HOME\.config"
 
-# simple back command
-function .. () { Set-Location .. }
-function home () { Set-Location $HOME/home }
+# Aliases
+Set-Alias -Name br -Value broot
+Set-Alias -Name mt -Value marktext
+Set-Alias -Name vim -Value nvim
+Set-Alias -Name foo -Value foobar2000
+Set-Alias -Name mail -Value mailspring
+Set-Alias -Name lw -Value "$env:ProgramFiles\LibreWolf\librewolf.exe"
 
-# Program ShortCut
-function lw () { Start-Process "$env:ProgramFiles\LibreWolf\librewolf.exe" $args }
-function fo () { Start-Process "$(scoop prefix foobar2000)\foobar2000.exe" }
-function manga () { Start-Process "$HOME\home\apps\NeeView39.3\NeeView.exe" }
+# Manga reader. See https://bitbucket.org/neelabo/neeview/wiki/Home
+Set-Alias -Name manga -Value "$HOME\home\apps\NeeView39.5-x64\NeeView.exe"
 
-# Update All Programs
-function up () { sudo scoop update *; sudo scoop update * --global }
+# Utility
+function .. { Set-Location .. }
+function home { Set-Location $HOME/home }
+function list { nvim $LIST }
+function up { sudo scoop update *; sudo scoop update * --global }
+function clh { nvim "$env:APPDATA/Microsoft/Windows/Powershell/PSReadLine/ConsoleHost_history.txt" }
 
-# Play music without video. uri will be video link or m3u8 playlist.
-function mnv ([string]$uri) {
-    mpv --no-video --no-resume-playback $args $uri
+# Check disk command
+function dismexe () { sudo Dism.exe /Online /Cleanup-Image /RestoreHealth }
+function scannow () { sudo sfc /scannow }
+
+# Alias for various command
+function ll {
+    # lsd with additional information
+    lsd --long --almost-all --dereference --group-directories-first --blocks date --blocks size --blocks name --blocks inode --date '+%Y/%m/%d %X' -I desktop.ini $args
 }
-
-# Shuffle playlist, then play music from playlist.
-function msfl () {
-    $pl = $(fd -t f . $HOME\home\music\m3u8)
-    $file = "D:\.cache\YoutubeMusic\_playlist.m3u8"
-    Get-Random $pl -Count $pl.Length > $file; mpv --no-video --playlist=$file
+function fzfp {
+    # Preview fzf result using bat
+    fzf --preview="bat --color=always --style=numbers --line-range=:500 {}"
 }
-
-# Edit history data
-function clh () { nvim "$env:APPDATA/Microsoft/Windows/Powershell/PSReadLine/ConsoleHost_history.txt" }
-
-# Alias for lsd
-function ll () {
-    lsd -lAL --blocks permission --blocks date --blocks size --blocks name --blocks inode --date '+%Y/%m/%d %X' -I desktop.ini $args
-}
-
-# Preview fzf result using bat
-function fzfp () {
-    fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}'
-}
-
-# monolith
-function mono ([string]$name = "index") {
-    monolith --no-js --no-frames --no-metadata --no-fonts --no-video --output D:/Downloads/$name.html $args
-}
-
-# Create symbolic link
-function sym ([string]$path, [string]$target) {
-    sudo New-Item -ItemType SymbolicLink -Path $path -Target $target
-}
-
-# Downloaders
-function yt ([string]$case = "hls") {
-    yt-dlp --config-location $CONFIG/yt-dlp/$case.conf
-}
-function gld () {
-    gallery-dl --input-file $LIST --destination D:/Pictures/gallery-dl --no-mtime $args
-}
-function aria () {
-    aria2c --conf-path=$CONFIG/aria2.conf -d "D:/Downloads" -x16 -j16 -s16 -k1M -i $LIST
-}
-
-# Open list.txt to edit URL for various downloaders
-function list () { nvim $LIST }
-
-# Open config file
-function Edit-MyConfig ([string]$filename = "ps") {
-    $case = @{
-        vim = 'nvim\init.vim'
-        mpv = 'mpv.conf'
-        ps  = 'Powershell\Microsoft.Powershell_profile.ps1'
-    }
-    nvim $CONFIG\$($case[$filename])
-}
-
-# Get process name if that has title
-function psn () {
+function psn {
+    # Get process name if that has title
     Get-Process | Where-Object { $_.mainWindowTitle } | Format-Table Id, Name, mainWindowtitle -AutoSize
 }
 
-# Check disk commands
-function dismexe () { sudo Dism.exe /Online /Cleanup-Image /RestoreHealth }
-function scannow () { sudo sfc /scannow }
+function mnv ([string]$uri) {
+    mpv --no-video --no-resume-playback $args $uri
+    <#
+        .SYNOPSIS
+        Play music without video.
+
+        .PARAMETER uri
+        URL or m3u8 playlist.
+
+        .EXAMPLE
+        mnv https://music.youtube.com/playlist?list=OLAK5uy_mwpOUfoLWybXalD2eksUMiwoi-_9a3aDA
+
+        .EXAMPLE
+        mnv --shuffle $HOME\home\music\m3u8\LOVEBITES\*.m3u8
+    #>
+}
+
+function msfl {
+    param (
+        [string[]]$source = $(fd -t f -e .m3u8 . $HOME\home\music\m3u8),
+        [bool]$all = $false
+    )
+    if ($all) {
+        param(
+            [string]$playlist = "D:\.cache\YoutubeMusic\_playlist.m3u8"
+        )
+        Get-Random $source -Count $source.Length > $playlist; mpv --no-video --playlist=$playlist
+    }
+    else {
+        mpv --no-video $($source | Get-Random)
+    }
+    <#
+        .SYNOPSIS
+        Shuffle playlist, then play music from the playlist.
+
+        .PARAMETER source
+        List of the m3u8 playlist path.
+        They are must be Full-Path.
+
+        .PARAMETER all
+        If you use `-all` option, it stores playlist in $playlist and plays music from the file.
+
+        .PARAMETER playlist
+        The path where to store temporally playlist file to play the music with mpv.
+        This param is also must be a Full-Path and can only used with `-all` option.
+
+        .EXAMPLE
+        msfl
+
+        This command plays a random album from default path.
+
+        .EXAMPLE
+        msfl -all -playlist "D:/tmp/tmp.m3u8"
+
+        Store playlists in "D:/tmp/tmp.m3u8", then mpv plays the music from the file.
+    #>
+}
+
+function mono {
+    param (
+        [Parameter(Mandatory)]
+        [string]$url,
+        [string]$filename = "index"
+    )
+    monolith --no-js --no-frames --no-metadata --no-fonts --no-video --output "D:/Downloads/$filename.html" $url
+    <#
+        .SYNOPSIS
+        Download HTML file from web site using monolith.
+
+        .PARAMETER url
+        URL of web site.
+
+        .PARAMETER filename
+        File name for download content.
+    #>
+}
+
+function sym {
+    param (
+        [Parameter(Mandatory)]
+        [string]$path,
+        [string]$target
+    )
+    sudo New-Item -ItemType SymbolicLink -Path $path -Target $target
+    <#
+        .SYNOPSIS
+        Create symbolic link.
+
+        .PARAMETER path
+        File path that you want to create symbolic link.
+
+        .PARAMETER target
+        Original file of symbolic link that already exists.
+
+        .EXAMPLE
+        sym D:/symlinks/profile.ps1 $PROFILE
+
+        Create sym link of $PROFILE in "D:/symlinks" and file name is "profile.ps1"
+    #>
+}
+
+function yt {
+    param(
+        [ValidateSet("hls", "audio", "norm", "yt", "max", "safe")]
+        $case = "hls"
+    )
+    yt-dlp --config-location "$CONFIG/yt-dlp/$case.conf"
+    <#
+        .SYNOPSIS
+        Download media from various web sites using yt-dlp.
+
+        .PARAMETER case
+        Set the config profile by use case. Default use case is "hls".
+
+        .EXAMPLE
+        yt audio
+
+        This command will start to download audio file listed in the $LIST.
+    #>
+}
+
+function aria {
+    param(
+        # Directory where you want to download content.
+        [string]$dir = "$HOME/home/videos/temp"
+    )
+    aria2c --conf-path="$CONFIG/aria2.conf" -d $dir -x6 -j6 -s6 -k1M -i $LIST
+}
+
+function gld {
+    gallery-dl --input-file $LIST --destination D:/Pictures/gallery-dl --no-mtime $args
+}
+
+function Edit-MyConfig {
+    param(
+        [ValidateSet("mpv", "vim", "ps")]
+        [string]$filename = "ps"
+    )
+    $case = @{
+        mpv = 'mpv.conf'
+        vim = 'nvim\init.vim'
+        ps  = 'Powershell\Microsoft.Powershell_profile.ps1'
+    }
+    nvim $CONFIG\$($case[$filename])
+    <#
+        .SYNOPSIS
+        Open specific config file in "~/.config".
+
+        .EXAMPLE
+        Edit-MyConfig mpv
+
+        It opens mpv config file (mpv.conf) in "~/.config".
+    #>
+}
 
 # Using utf-8 encoding to write data with directors: ">" or ">>"
 $PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'
@@ -133,11 +239,14 @@ Set-PSReadLineOption -AddToHistoryHandler {
     param ($command)
     switch -regex ($command) {
         "SKIPHISTORY" { return $false }
-        "^[a-z]$" { return $false }
-        "exit" { return $false }
-        "cls" { return $false }
-        "ll" { return $false }
+        "^[a-z]$" { return $false } # a single word
+        "es" { return $false }
+        "mv" { return $false }
+        "rm" { return $false }
+        "del" { return $false }
         "ren" { return $false }
+        "mpv" { return $false }
+        "ffprobe" { return $false }
     }
     return $true
 }
